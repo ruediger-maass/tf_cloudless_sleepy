3e2c9e18341a7aa7c99f7bb3ee10a2b353e31217
ruediger maass <ruediger.maass@de.ibm.com>
2017-05-31 22:48:50 +0200

    correct again

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 # Create an arbitrary local resource
-data "xtemplate_file" "test" {
+data "template_file" "test" {
   template = "Hello, I am a template. My sample_var value = $${sample_var}, terraform_tfvars_found = $${terraform_tfvars_found}"
 
   vars {
