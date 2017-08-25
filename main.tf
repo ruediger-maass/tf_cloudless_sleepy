459e00d0e5ef63cccb411e4987345b709f831b43
ruediger maass <ruediger.maass@de.ibm.com>
2017-08-25 10:06:59 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # Create an arbitrary local resource
 data "template_file" "test" {
-  template = "<iframe src=\"http://free.timeanddate.com/clock/i28mkpbc/tlde3/tct/pct/tt0\" frameborder=\"0\" width=\"277\" height=\"18\" allowTransparency=\"true\"></iframe> Hello, I am a template. My sample_var value = $${sample_var}, terraform_tfvars_found = $${terraform_tfvars_found}"
+  template = "<b>Hello, I am a template. My sample_var value = $${sample_var}, terraform_tfvars_found = $${terraform_tfvars_found}"
 
   vars {
     sample_var = "${var.sample_var}"
