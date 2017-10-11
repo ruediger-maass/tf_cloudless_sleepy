18b53823fe96c35c2c386cf02acce4a417f620ac
ruediger maass <ruediger.maass@de.ibm.com>
2017-10-11 23:51:05 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-# Create an arbitrary local resource
-data "template_file" "test" {
-  template = <<EOT
-sample_var value = "${var.sample_var}", 
-terraform_tfvars_found = "${var.terraform_tfvars_found}"
-EOT
-}
-
 resource "null_resource" "sleep" {
   triggers {
     uuid = "${uuid()}"
