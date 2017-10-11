feee73a9a833c765d415938b43a27ae8899e5536
ruediger maass <ruediger.maass@de.ibm.com>
2017-10-11 23:44:25 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,11 @@
 # Create an arbitrary local resource
 data "template_file" "test" {
   template = <<EOT
-Hello, I am a template. MASTER! My sample_var value = $${sample_var}, terraform_tfvars_found = $${terraform_tfvars_found}
+sample_var value = "${var.sample_var}", 
+sample_list = "${var.sample_list}"
+sample_map = "${var.sample_map}"
+terraform_tfvars_found = "${var.terraform_tfvars_found}"
 EOT
-  vars {
-    sample_var = "${var.sample_var}"
-    sample_list = "${var.sample_list}"
-    sample_map = "${var.sample_map}"
-    terraform_tfvars_found = "${var.terraform_tfvars_found}"
-  } 
 }
 
 resource "null_resource" "sleep" {
