fa3f25bc65da453feeaa2b83a5d2c0f21ae9e3fe
ruediger maass <ruediger.maass@de.ibm.com>
2017-10-11 23:48:11 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,6 @@
 data "template_file" "test" {
   template = <<EOT
 sample_var value = "${var.sample_var}", 
-sample_list = "${var.sample_list}"
-sample_map = "${var.sample_map}"
 terraform_tfvars_found = "${var.terraform_tfvars_found}"
 EOT
 }
