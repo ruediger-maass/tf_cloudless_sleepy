27badf05faac41e3ebf98d0af361e91463171fed
ruediger maass <ruediger.maass@de.ibm.com>
2017-12-04 12:39:33 +0000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,3 @@
 output "sample_var value" {
   value = "${var.sample_var}"
 }
-output "sample_list" {
-  value = "${var.sample_list}"
-}
-output "sample_map" {
-  value = "${var.sample_map}"
-}
-output "terraform_tfvars_found" {
-  value = "${var.terraform_tfvars_found}"
-}
