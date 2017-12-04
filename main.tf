5e262c77c7703a743cb7da94885b1b80a79b17fc
ruediger maass <ruediger.maass@de.ibm.com>
2017-12-04 12:39:01 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,14 @@
+# Create an arbitrary local resource
+data "template_file" "test" {
+  template = "Hello, I am a template. My sample_var value = ${var.sample_var}. The sleep time is ${var.sleepy_time}"
+}
+
 resource "null_resource" "sleep" {
   triggers {
     uuid = "${uuid()}"
   }
 
   provisioner "local-exec" {
-    command = "set" 
+    command = "sleep ${var.sleepy_time}"
   }
 }
